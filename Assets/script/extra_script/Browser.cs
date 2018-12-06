91afb9fd78c799afb64d2d52c0640cd89f58cc4a
Rajon Kumar Saha <srajonkumar@gmail.com>
2018-12-06 16:23:02 +0100

    browser update
    Friday 6 december

---
+++ Assets/script/extra_script/Browser.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Browser : MonoBehaviour
+{
+
+
+	public GameObject BrowserCanvas;
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(GetBrowser());
+		
+	}
+	
+	IEnumerator GetBrowser()
+	{
+
+		var _request = UnityWebRequest.Get("http://192.168.2.161/");
+		yield return _request.SendWebRequest();
+		if(_request.isNetworkError)
+		{
+			Debug.Log("Error: " + _request.error); 
+		}
+
+		else
+		{
+			AssetBundle bundel =DownloadHandlerAssetBundle.GetContent(_request);
+			Debug.Log("Received " + bundel);
+	
+		}
+
+	}
+
+
+	
+}
---
+++ Assets/script/extra_script/WebBrowser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WebBrowser : MonoBehaviour {
+
+	public GameObject Mycan;
+
+	//function declear
+	public void OpenIp()
+
+	{
+
+		Application.OpenURL("http://192.168.2.161/");
+		Debug.Log("Its working");
+	}
+
+	
+
+	}
